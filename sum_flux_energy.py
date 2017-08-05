65ff660b1187d6a714701dba0b89ea8c2aabf4d0
wschreyer <w.schreyer@tum.de>
2017-08-05 15:46:36 -0700

    Improved scripts for cluster runs

--- sum_flux_energy.py
+++ sum_flux_energy.py
@@ -30,17 +30,17 @@ for line in fileinput.input(glob.glob('*.out')):
   columns = line.split()
   if len(columns) == 7:
     try:
-      if int(columns[0]) == 14 and columns[1] == 'HEII' and columns[2] == '1.000000000D+00':
-        heII_heat.append(float(columns[3].replace('D', 'E'))*1.e9*1.6022e-19*6.2415e12*1000.) # get energy deposition into He-II and convert from GeV/proton to mW/uA
+      if int(columns[0]) == 14 and columns[1] == 'HEII' and (columns[2] == '1.000000000D+00' or columns[2] == '1.000000000E+00'):
+        heII_heat.append(float(columns[3].replace('D', 'E'))*1.e9*1e-6*1000.) # get energy deposition into He-II and convert from GeV/proton to mW/uA
         print '{}: {}'.format(fileinput.filename(), line)
-      elif int(columns[0]) == 13 and columns[1] == 'HEIIBOTT' and columns[2] == '1.000000000D+00':
-        heIIbottle_heat.append(float(columns[3].replace('D', 'E'))*1.e9*1.6022e-19*6.2415e12*1000.) # get energy deposition into He-II bottle and convert from GeV/proton to mW/uA
+      elif int(columns[0]) == 13 and columns[1] == 'HEIIBOTT' and (columns[2] == '1.000000000D+00' or columns[2] == '1.000000000E+00'):
+        heIIbottle_heat.append(float(columns[3].replace('D', 'E'))*1.e9*1e-6*1000.) # get energy deposition into He-II bottle and convert from GeV/proton to mW/uA
         print '{}: {}'.format(fileinput.filename(), line)
     except ValueError:
       continue
 fileinput.close()
 
-print 'cold neutron (<2meV) flux: {0:.3g} +- {1:.3g} 1/(cm2 s uA)'.format(totalflux, dflux)
-print 'heat input into He-II: {0:.2f} +- {1:.2f} mW/uA'.format(numpy.mean(heII_heat), numpy.std(heII_heat))
-print 'heat input into He-II bottle: {0:.2f} +- {1:.2f} mW/uA'.format(numpy.mean(heIIbottle_heat), numpy.std(heIIbottle_heat))
+print 'cold neutron flux (<2meV) in He-II:\n{0:.3g} +- {1:.2g} 10^12/(cm2 s uA)\n'.format(totalflux/1e12, dflux/1e12)
+print 'energy deposition in He-II:\n{0:.3g} +- {1:.2g} mW/uA\n'.format(numpy.mean(heII_heat), numpy.std(heII_heat))
+print 'energy deposition in He-II bottle:\n{0:.3g} +- {1:.2g} mW/uA\n'.format(numpy.mean(heIIbottle_heat), numpy.std(heIIbottle_heat))
 
