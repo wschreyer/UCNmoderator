1132b93a7e11712ee0cbb3d22e8aacb3f9886220
Wolfgang Schreyer <w.schreyer@tum.de>
2018-01-30 23:55:29 -0500

    Implemented horizontal-source design from 2014

--- README.md
+++ README.md
@@ -1,43 +1,27 @@
-Simulation for Phase II UCN source.
-
-Tungsten target with water jacket, encased in lead.
-Cylindrical D2O (296 K), LD2 (24 K), and He-II (1 K) vessels offset 0 cm from target.
-Sides of D2O vessel covered with graphite reflectors.
-
-Distances above target (cm) + vessel wall thickness:
-Lead - D2O: 11.8 + 0.5
-D2O - LD2: 15 + 0.5 + 1 + 0.5
-LD2 - HE-II: 8 + 0.3 + 0.8 + 0.2
-
-Outer vessel sizes (cm) - height, radius:
-D2O: 82.5 43
-LD2: 51 33
-He-II: 33 16
-
 UCN production in He-II:
-6.61e+05 +- 1.9e+03 1/(s uA)
+3.42e+05 +- 1.1e+03 1/(s uA)
 
-prompt energy deposition in He-II (20.6 l, 2.99 kg):
-71.3 +- 0.58 mW/uA
+prompt energy deposition in He-II (10.9 l, 1.57 kg):
+49.4 +- 0.44 mW/uA
 
-prompt energy deposition in He-II bottle (0.844 l, 2.28 kg):
-51.5 +- 0.4 mW/uA
+prompt energy deposition in He-II bottle (0.644 l, 1.74 kg):
+48 +- 0.4 mW/uA
 
 delayed energy deposition in He-II:
-min 4.5 +- 0.07 mW/uA, max 11.4 +- 0.12 mW/uA
+min 3.1 +- 0.057 mW/uA, max 8.02 +- 0.096 mW/uA
 
 delayed energy deposition in He-II bottle:
-min 13.3 +- 0.12 mW/uA, max 33.1 +- 0.19 mW/uA
+min 10.2 +- 0.11 mW/uA, max 24.9 +- 0.17 mW/uA
 
-prompt energy deposition in LD2 (115 l, 18.5 kg):
-535 +- 1.9 mW/uA
+prompt energy deposition in LD2 (136 l, 21.7 kg):
+828 +- 2.4 mW/uA
 
-prompt energy deposition in LD2 bottle (9.18 l, 24.8 kg):
-483 +- 1.5 mW/uA
+prompt energy deposition in LD2 bottle (18.2 l, 49.2 kg):
+1.14e+03 +- 2.8 mW/uA
 
 delayed energy deposition in LD2:
-min 19.6 +- 0.15 mW/uA, max 47.3 +- 0.24 mW/uA
+min 22.7 +- 0.16 mW/uA, max 54.9 +- 0.25 mW/uA
 
 delayed energy deposition in LD2 bottle:
-min 95.9 +- 0.35 mW/uA, max 231 +- 0.58 mW/uA
+min 163 +- 0.48 mW/uA, max 390 +- 0.79 mW/uA
 
--- writeREADME.py
+++ writeREADME.py
@@ -7,31 +7,6 @@ import readResults
 surfs = readResults.ReadSurfaces(io.FileIO('out1'))
 cells = readResults.ReadCells(io.FileIO('out1'))
 
-print 'Simulation for Phase II UCN source.\n'
-print 'Tungsten target with water jacket, encased in lead.'
-target_offset = surfs[25]['size'][0] + 0.04
-print 'Cylindrical D2O ({0:.3g} K), LD2 ({1:.3g} K), and He-II ({2:.3g} K) vessels offset {3:.3g} cm from target.'.format(cells[16]['temp'], cells[18]['temp'], cells[21]['temp'], target_offset)
-print 'Sides of D2O vessel covered with graphite reflectors.\n'
-print 'Distances above target (cm) + vessel wall thickness:'
-lead_cover = surfs[5]['size']
-ld2o_bottle = surfs[53]['size']
-ld2o = surfs[52]['size']
-print 'Lead - D2O: {0:.3g} + {1:.3g}'.format(ld2o_bottle[2] - lead_cover[4], ld2o[2] - ld2o_bottle[2])
-vac_tank = surfs[71]['size']
-vac = surfs[70]['size']
-ld2_bottle = surfs[55]['size']
-ld2 = surfs[56]['size']
-print 'D2O - LD2: {0:.3g} + {1:.3g} + {2:.3g} + {3:.3g}'.format(vac_tank[2] - ld2o[2], vac[2] - vac_tank[2], ld2_bottle[2] - vac[2], ld2[2] - ld2_bottle[2])
-ld2_ibottle = surfs[57]['size']
-heii_vac = surfs[58]['size']
-heii_bottle = surfs[68]['size']
-heii = surfs[69]['size']
-print 'LD2 - HE-II: {0:.3g} + {1:.3g} + {2:.3g} + {3:.3g}\n'.format(ld2_ibottle[2] - ld2[2], heii_vac[2] - ld2_ibottle[2], heii_bottle[2] - heii_vac[2], heii[2] - heii_bottle[2])
-print 'Outer vessel sizes (cm) - height, radius:'
-print 'D2O: {0:.3g} {1:.3g}'.format(ld2o_bottle[5], ld2o_bottle[6])
-print 'LD2: {0:.3g} {1:.3g}'.format(vac_tank[5], vac_tank[6])
-print 'He-II: {0:.3g} {1:.3g}\n'.format(ld2_ibottle[5], ld2_ibottle[6])
-
 tallies_file = ROOT.TFile('tallies.root', 'READ')
 print 'UCN production in He-II:\n{0[0]:.3g} +- {0[1]:.2g} 1/(s uA)\n'.format(readResults.GetUCNProduction(tallies_file, 21))
       
