0315ca84790d0a15e5446b53f9faf523b5be1fdd
wschreyer <wschreyer@tum.de>
2018-03-02 10:37:18 -0800

    Rotated source, added new assays of graphite, SS Guide now orthogonal to beam

--- postrun.sh
+++ postrun.sh
@@ -11,7 +11,7 @@ WD=/home/wschreye/scratch/flukasims
 { ls ${WD}/ucn*_fort.22; echo ; echo ucn_22.bnn; } | $FLUPRO/flutil/usbsuw
 { echo ucn_22.bnn; echo ucn_22.bnn.asc; } | $FLUPRO/flutil/usbrea
 #rm i${WD}/ucn*_fort.22
-#{ ls ucn_*_fort.23; echo ; echo ucn_23.rnc; } | $FLUPRO/flutil/usrsuw
+{ ls ${WD}/ucn*_fort.23; echo ; echo ucn_23.rnc; } | $FLUPRO/flutil/usrsuw
 #rm ucn_*_fort.23
 { ls ${WD}/ucn*_fort.24; echo ; echo ucn_24.bnn; } | $FLUPRO/flutil/usbsuw
 #rm ${WD}/ucn*_fort.24
--- usimbs.f
+++ usimbs.f
@@ -50,11 +50,11 @@
 
       Y1 = Ytrack(0)
       Y2 = Ytrack(Ntrack)
-      IF ( Y1 .GT. -60. ) Y1 = -60.
+      IF ( Y1 .GT. -75. ) Y1 = -75.
       IF ( Y1 .LT. -130. ) Y1 = -130.
-      IF ( Y2 .GT. -60. ) Y2 = -60.
+      IF ( Y2 .GT. -75. ) Y2 = -75.
       IF ( Y2 .LT. -130. ) Y2 = -130.
-      FIMP = EXP( (Y1 - Y2)/7 )
+      FIMP = EXP( (Y1 - Y2)/5.5 )
 
       RETURN
 *
