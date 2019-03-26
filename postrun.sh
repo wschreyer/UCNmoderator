d1b71f0d9c47c1cee62a159132ed8208d0eae7e7
Wolfgang Schreyer <wschreye@is16.m>
2019-03-26 02:19:26 -0400

    Dose rates of extracted target

--- postrun.sh
+++ postrun.sh
@@ -9,7 +9,6 @@ WD=/home/wschreye/scratch
 { ls ${WD}/ucn*_fort.21; echo ; echo ucn_21.trk; } | $FLUPRO/flutil/ustsuw
 #rm ${WD}/ucn*_fort.21
 { ls ${WD}/ucn*_fort.22; echo ; echo ucn_22.bnn; } | $FLUPRO/flutil/usbsuw
-{ echo ucn_22.bnn; echo ucn_22.bnn.asc; } | $FLUPRO/flutil/usbrea
 #rm i${WD}/ucn*_fort.22
 { ls ${WD}/ucn*_fort.23; echo ; echo ucn_23.rnc; } | $FLUPRO/flutil/usrsuw
 { ls ${WD}/ucn*_fort.25; echo ; echo ucn_25.rnc; } | $FLUPRO/flutil/usrsuw
