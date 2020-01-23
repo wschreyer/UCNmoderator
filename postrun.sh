d1433a13c6bf51d3bf8041277f44f66bedc830d7
wschreyer <wschreyer@tum.de>
2020-01-23 00:46:02 -0800

    Added boron nitride at penetration

--- postrun.sh
+++ postrun.sh
@@ -15,6 +15,7 @@ WD=/home/wschreye/scratch
 { ls ${WD}/ucn*_fort.25; echo ; echo ucn_25.rnc; } | $FLUPRO/flutil/usrsuw &
 #rm ucn_*_fort.23
 { ls ${WD}/ucn*_fort.24; echo ; echo ucn_24.bnn; } | $FLUPRO/flutil/usbsuw &
+{ ls ${WD}/ucn*_fort.26; echo ; echo ucn_26.bnn; } | $FLUPRO/flutil/usbsuw &
 #rm ${WD}/ucn*_fort.24
 #head README.md -n16 > READMEn.md
 #python sum_flux_energy.py | tail -n27 >> READMEn.md
